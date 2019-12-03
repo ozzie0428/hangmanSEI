fc5543670426fed3711d433b1f03716df4e68e4c
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2019-12-03 14:27:37 -0500

    create empty array for right and wrong letters and push letters into them

--- app.js
+++ app.js
@@ -50,10 +50,20 @@ let chosenPalindrome = palindromeWords[random];
     // convert key numbers into a srting.
     let keyWord = String.fromCharCode(keys); 
     // console.log(keyWord); 
+    if(chosenSei.indexOf(keyWord) > -1) {
+        // console.log(true)
+        correctLetters.push(keyWord); 
+        console.log(correctLetters);
+    } else {
+        wrongLetters.push(keyWord);
+        console.log(wrongLetters); 
+    }
  })
 
-// check if user is correct 
 // if user is correct, push to correct section 
+let correctLetters = [];
+
 // if user is wrong, push to incorrect secrion 
+let wrongLetters = [];
 // be able to count guess left
 // get submit button reshuffle the words
