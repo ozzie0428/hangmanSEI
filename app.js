259a275423617eccbc4c30e80280dd1de2b04f63
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2019-12-02 21:56:17 -0500

    add puesdo code

---
+++ app.js
@@ -0,0 +1,9 @@
+// create promt for user name
+
+// create array of words
+// choose random words 
+// create underscore based on length of words
+// get users submission 
+// check if user is correct 
+// if user is correct, push to correct section 
+// if user is wrong, push to incorrect secrion 
