42e2aa4f6dfe0e9bdb0bb9ab5ad22ae57c4d0611
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2019-12-03 18:36:58 -0500

    fix arrays

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function namePromt() {
 //     sei: ['function','array','for loop','while loop','variables','methods','html','javascript','css','objects','events','if','else']
 // }
 // create array of words for sports
-const sportsWords = ['Football','Futbol','penalty','foul','goal','touchdown','helmet','jersey','kit','cleats','giants','knicks','liverpool'];
+const sportsWords = ['football','futbol','penalty','foul','goal','touchdown','helmet','banana','kit','cleats','giants','knicks','liverpool'];
 
 // create array of words for SEI
 const seiWords = ['function','array','for loop','while loop','variables','methods','html','javascript','css','objects','events','if','else'];
@@ -23,26 +23,31 @@ const seiWords = ['function','array','for loop','while loop','variables','method
 const palindromeWords = ['level','Hannah','civic','kayak','madam','racecar','stats','mom','Noon','peep','sooloos','deedeed','kakkak'];
 
 // choose random words 
-let random = Math.floor(Math.random() * seiWords.length); 
+let random = Math.floor(Math.random() * sportsWords.length); 
 
 let chosenSports =  sportsWords[random];
-// console.log(chosenSports);
+console.log(chosenSports);
 
 let chosenSei = seiWords[random];
-console.log(chosenSei);
+// console.log(chosenSei);
 
 let chosenPalindrome = palindromeWords[random];
 // console.log(chosenPalindrome);
+// if user is correct, push to correct section 
+let correctLetters = [];
+
+// if user is wrong, push to incorrect secrion 
+let wrongLetters = [];
 
  // create underscore based on length of words
  let guessedLetter = []
  function blankLetter() {
-    for (let i = 0; i <  chosenSei.length; i++) {
+    for (let i = 0; i <  chosenSports.length; i++) {
         guessedLetter.push('_');
     }
     return guessedLetter;
  }
- console.log(blankLetter());
+//  console.log(blankLetter());
 // get users submission 
  document.addEventListener('keypress',(event)=> {
     //  console.log(event)  
@@ -50,27 +55,24 @@ let chosenPalindrome = palindromeWords[random];
     // convert key numbers into a srting.
     let keyWord = String.fromCharCode(keys); 
     // console.log(keyWord); 
-    if(chosenSei.indexOf(keyWord) > -1) {
-        // console.log(true)
+    if(chosenSports.indexOf(keyWord) > -1) {
+        console.log(true)
         correctLetters.push(keyWord); 
-        // console.log(correctLetters);
-    } 
-    guessedLetter[chosenSei.indexOf(keyWord)] = keyWord;
-    if (guessedLetter.join('') == chosenSei) {
+        console.log(correctLetters);
+        guessedLetter[chosenSports.indexOf(keyWord)] = keyWord;
+    if (guessedLetter == chosenSports) {
         alert('Congrats, you won!');
     }
+    } 
+    
     else {
         wrongLetters.push(keyWord);
-        // console.log(wrongLetters);  
+        console.log(wrongLetters);  
     }
 
  })
 
-// if user is correct, push to correct section 
-let correctLetters = [];
 
-// if user is wrong, push to incorrect secrion 
-let wrongLetters = [];
 
 // replace underscore with letters
 
